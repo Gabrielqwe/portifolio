689ac400f027953d1c23fcd38b6b7a1b4f4e8aaa
gabrieqwe <gabriel.ferreira.lopes159@gmail.com>
2023-06-10 15:01:06 -0300

    atualização

--- java.js
+++ java.js
@@ -1,26 +1,22 @@
-function escrevendoletra(){
+function escrevendoletra() {
+    function ativarletra(elemento) {
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i) => {
+            setTimeout(() => {
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
 
-   function ativarletra(elemento) {
-    const arrTexto = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i)=>{
-        setTimeout(()=>{
-            elemento.innerHTML += letra;
-        },75 * i);
-    });
     const titulo = document.querySelector('.digitando');
-ativarletra(titulo);
-
-}
-
-
-
+    ativarletra(titulo);
 }
 
 const ativamenu = document.querySelector('.fa-bars');
+const navmenu = document.querySelector('header .navegação-primaria');
 
-ativamenu.addEventListener('click', ()=>{
+ativamenu.addEventListener('click', () => {
     ativamenu.classList.toggle('fa-x');
-
-})
-
+    navmenu.classList.toggle('ativado');
+});
